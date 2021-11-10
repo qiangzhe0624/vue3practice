006313fe367236429f08bb5ea20e7b832a415901
qianghzhe0624 <1836571939@qq.com>
2021-11-10 18:25:55 +0800

    'vue3.0初体验'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,75 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input type="number" v-model="state.num1"><span>+</span>
+    <input type="number" v-model="state.num2"><span>=</span>
+    <span>{{state.result}}</span>
+    <button @click="clickData">子传父</button>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
+<script>
+// import { ref } from 'vue'
+import { reactive,computed,onMounted,onRenderTracked,onRenderTriggered,onUpdated } from 'vue'
+export default {
   props: {
-    msg: String
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate');
+  },
+  created(){
+    console.log('created');
+  },
+  mounted(){
+    console.log('mounted');
+  },
+  // setup (props) {
+  //   console.log(props) // { user: '' }
+  //   const num1 = ref(0)
+  //   const num2 = ref(0)
+  //   const result = ref(0)
+  //   function add(){
+  //     result.value=num1.value+num2.value
+  //   }
+  //   return {
+  //     num1,num2,result,add
+  //   } // 这里返回的任何内容都可以用于组件的其余部分
+  // }
+  setup(props, {emit}){
+    console.log(props,'props');
+    const state = reactive({
+      num1:0,
+      num2:0,
+      result:computed(()=>state.num2+state.num1)
+    })
+    onMounted(() => {
+      console.log('Component is mounted!')
+    })
+    onRenderTracked((e) => {
+      console.log('Component is onRenderTracked!',e)
+    })
+    onRenderTriggered((e) => {
+      console.log('Component is onRenderTriggered!',e)
+    })
+    onUpdated(() => {
+      console.log('Component is onUpdated!')
+    })
+    // function add() {
+    //   state.result = state.num1+state.num2
+    // }
+    function clickData(){
+      console.log('clickData');
+      emit('msg',state.result)
+    }
+    return {
+      state,
+      clickData
+    }
   }
-})
-export default class HelloWorld extends Vue {
-  msg!: string
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" @msg="getMsg"/>
   </div>
+  强者
 </template>
 
 <script lang="ts">
@@ -13,6 +14,16 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   components: {
     HelloWorld,
   },
+  setup(){
+    console.log('homesetup');
+    
+  },
+  methods:{
+    getMsg(result:number){
+      console.log(result,'result');
+      
+    }
+  }
 })
 export default class Home extends Vue {}
 </script>
